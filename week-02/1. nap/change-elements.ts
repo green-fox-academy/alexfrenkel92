fda6644c7f8ea62a25d26812453f9fc5246be57f
alexfrenkel92 <alexfrenkel92@gmail.com>
2019-12-09 15:01:00 +0100

    increment, appends

---
+++ week-02/1. nap/change-elements.ts
@@ -0,0 +1,14 @@
+var numList: number[] = [1, 2, 3, 8, 5, 6];
+var root = numList.map(Math.sqrt);
+
+console.log(root);
+
+
+// var numbers = [1, 4, 9]; 
+// var roots = numbers.map(Math.sqrt); 
+// console.log("roots is : " + roots );
+
+
+// -  Create an array named `numList` with the following content: `[1, 2, 3, 8, 5, 6]`
+// -  Change the 8 to 4 with the `.map` method 
+// -  Print the fourth element as a test
