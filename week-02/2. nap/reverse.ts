25868e3c8cc9aa684132cffd248fe9cb639eac5f
alexfrenkel92 <alexfrenkel92@gmail.com>
2019-12-10 13:45:43 +0100

    reverse

---
+++ week-02/2. nap/reverse.ts
@@ -0,0 +1,9 @@
+'use strict';
+// Create a function that can reverse a string, which is passed as the parameter
+// Use it on this reversed string to check it!
+
+let reversed: string = `.eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI`;
+let solution: string[] = reversed.split('');
+let fs: string[] = solution.reverse();
+
+console.log(fs.join(''));
