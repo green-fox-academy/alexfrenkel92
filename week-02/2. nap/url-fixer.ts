a4efb57026430c10919644aaab2182a1cb9c3ba4
alexfrenkel92 <alexfrenkel92@gmail.com>
2019-12-10 14:30:30 +0100

    takes-longer

--- week-02/2. nap/url-fixer.ts
+++ week-02/2. nap/url-fixer.ts
@@ -6,12 +6,6 @@ let url: string = 'https//www.reddit.com/r/nevertellmethebots';
 let url1: string = url.replace('bots', 'odds');
 let url2: string[] = url1.split('');
 
-console.log(url2.splice(5, 0, ':'));
-console.log(url2.join(''));
-
-// console.log(example.replace('Reddit', 'Reboot')); //mit mire cserelunk
-// let newString: string = example.replace('Reddit', 'Awesome');
-// console.log(newString);
-
-// console.log(bestArrayEver.splice(1, 0, 68)); //1es indexen kezd; 0 azt jelenti, nem kell semmit kitorolnie; 68-t hozzaadja 1 index utan
-// console.log(bestArrayEver);
+url2.splice(5, 0, ':');
+url = url2.join('');
+console.log(url);
