71ad5a6ef46dbbde99314b0d9ff884d851a62fe3
alexfrenkel92 <alexfrenkel92@gmail.com>
2019-12-19 16:57:26 +0100

    bunnies again

---
+++ week-03/4. nap/bunnies-again.ts
@@ -0,0 +1,17 @@
+'use strict'
+//We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say have 3 ears, because they each have a raised foot. Recursively return the number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+
+function bunnyEars1 (n): number {
+    let earsOdd: number = 1;
+    let earsEven: number = 2;
+    console.log(n);
+    if (n == 1) {
+        return n;
+    }
+    if (n % 2 === 0) {
+    return bunnyEars1(n - 1) + earsEven;
+    } else
+    return bunnyEars1(n - 1) + earsOdd;
+}
+
+console.log('Total number of bunny ears: ' + bunnyEars1(2));
--- week-03/4. nap/bunnies.ts
+++ week-03/4. nap/bunnies.ts
@@ -15,7 +15,7 @@ console.log('Total number of bunny ears: ' + bunnyEars(6));
 
 // function nSum(N) {
 //     console.log(N)
-//     if (N == 1) {
+//     if (N == 1) {                        
 //         return N;
 //     }
 //     return nSum(N - 1) + N;
