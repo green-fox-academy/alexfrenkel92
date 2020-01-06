8d028c3d363e1eea8683eb1d0851ed44380d93cc
alexfrenkel92 <alexfrenkel92@gmail.com>
2020-01-06 15:51:37 +0100

    anagram done

--- week-04/1. nap/getindex.ts
+++ week-04/1. nap/getindex.ts
@@ -2,7 +2,6 @@
 //Create a function that takes a list and a value, and returns the index of that value in the list. If the value is not it the list, the function should return -1.
 
 function getIndex (index): string {
-    // let list: string[];
     let list = ['a', 'b', 'c', 'd', 'e', 'f'].indexOf(index);
     // let list1 = [1, 2, 3, 4, 5, 6].indexOf(index);
     // console.log('Index is: ' + list1)
@@ -11,6 +10,3 @@ function getIndex (index): string {
 }
 
 console.log(getIndex('d'));
-
-// let index = [12, 5, 8, 130, 44].indexOf(8); 
-// console.log("index is : " + index );
