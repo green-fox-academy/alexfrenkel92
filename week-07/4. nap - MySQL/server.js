6a1c6121b0f30f5d06cf59fc754a01d71b8a1b96
alexfrenkel92 <alexfrenkel92@gmail.com>
2020-01-31 10:58:02 +0100

    bookstore full data ready

--- week-07/4. nap - MySQL/server.js
+++ week-07/4. nap - MySQL/server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const mysql = require('mysql');
+const path = require('path');
 
 let conn = mysql.createConnection({
     host: process.env.DB_HOST,
@@ -22,13 +23,6 @@ conn.connect((err) => {
     // conn.end();
 });
 
-// app.get('/', (req, res) => {
-//     conn.query('SELECT * FROM author', function (err, rows) {
-//         console.log(rows);
-//     })
-//     res.send(rows);
-// });
-
 app.get('/', (req, res) => {
     conn.query('SELECT * FROM author;', (err, rows) => {
         if (err) {
@@ -39,6 +33,40 @@ app.get('/', (req, res) => {
         res.send(rows);
     });
 });
+
+app.get('/booktitles', (req, res) => {
+    conn.query('SELECT book_name FROM book_mast;', (err, rows) => {
+        if (err) {
+            console.error(`Cannot retrieve data: ${err.toString()}`);
+            res.sendStatus(500);
+            return null;
+        }
+        res.json(rows);
+    });
+});
+
+app.get('/fulldata', (req, res) => {
+    conn.query(
+        `SELECT
+            book_mast.book_name, author.aut_name, category.cate_descrip, newpublisher.pub_name, book_mast.book_price
+        FROM
+            author
+        INNER JOIN book_mast ON author.aut_id = book_mast.aut_id
+        INNER JOIN category ON book_mast.cate_id = category.cate_id
+        INNER JOIN newpublisher ON newpublisher.pub_id = book_mast.pub_id;`,
+        (err, rows) => {
+            if (err) {
+                console.error(`Cannot retrieve data: ${err.toString()}`);
+                res.sendStatus(500);
+                return null;
+            }
+            res.json(rows);
+        });
+});
+
+
+
+
 
 app.listen(PORT, () => {
     console.log('Server is running on PORT 3000');
