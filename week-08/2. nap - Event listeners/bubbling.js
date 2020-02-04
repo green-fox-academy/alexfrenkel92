798e0e031ac65302987b3608bb40b76050c99ad0
alexfrenkel92 <alexfrenkel92@gmail.com>
2020-02-04 17:30:19 +0100

    bubbling terrorist

--- week-08/2. nap - Event listeners/bubbling.js
+++ week-08/2. nap - Event listeners/bubbling.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const nav = document.querySelector('nav');
+const imgInspector = document.querySelector('.img-inspector');
+let size = 500;
+let posX = 0;
+let posY = 0;
+
+nav.addEventListener('click', navigate);
+
+function navigate(e) {
+    const position = e.target.dataset.direction;
+    if (position === 'up') {
+        posY -= 10;
+        imgInspector.style.backgroundPositionY = posY + 'px';
+    };
+    if (position === 'down') {
+        posY += 10;
+        imgInspector.style.backgroundPositionY = posY + 'px';
+    };
+    if (position === 'left') {
+        posX -= 10;
+        imgInspector.style.backgroundPositionX = posX + 'px';
+    };
+    if (position === 'right') {
+        posX += 10;
+        imgInspector.style.backgroundPositionX = posX + 'px';
+    };
+    if (position === 'in') {
+        size += 20;
+        imgInspector.style.backgroundSize = size + 'px';
+    };
+    if (position === 'out') {
+        size -= 20;
+        imgInspector.style.backgroundSize = size + 'px';
+    };
+};
