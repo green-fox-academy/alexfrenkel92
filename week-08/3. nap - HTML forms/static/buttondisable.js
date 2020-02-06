9cac89ff1148613a3b6cb24eea7d342dc9d5cbd4
alexfrenkel92 <alexfrenkel92@gmail.com>
2020-02-06 09:20:12 +0100

    bbutton disable done, but not as required

--- week-08/3. nap - HTML forms/static/buttondisable.js
+++ week-08/3. nap - HTML forms/static/buttondisable.js
@@ -1,12 +1,33 @@
-// 'use strict';
+'use strict';
 
-// const question = document.querySelector('#question');
+const question = document.querySelector('#question');
+const favPet = document.querySelectorAll('.favouritePet');
+const signup = document.querySelector('#signup');
+const loveCatsButton = document.querySelector('#loveCats');
 
-// question.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     document.querySelectorAll('input[type="radio"]').forEach(item => {
-//         if(item.checked) {
-//             console.log(item)
-//         }
-//     })
-// })
+document.querySelectorAll('input[name="animal"]').forEach(item => {
+    item.addEventListener('input', (e) => {
+        e.preventDefault();
+        if (e.target.value == "cat" == true) {
+            console.log('cat is checked');
+            // console.log(signup);
+            signup.style.display = 'block';
+        } else if (e.target.value != "cat") {
+            signup.style.display = 'none';
+            alert(`That's not a cat`)
+        }
+    })
+})
+
+document.querySelectorAll('input[name="facts"]').forEach(item => {
+    item.addEventListener('input', (e) => {
+        e.preventDefault();
+        if (e.target.value == "yes") {
+            console.log(loveCatsButton);
+            loveCatsButton.style.display = 'block';
+            alert('Thank you for your subscription!')
+        } else if (e.target.value == 'no') {
+            alert('You should really change your mind...')
+        }
+    })
+})
