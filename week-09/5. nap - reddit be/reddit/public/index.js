67607d3d3692022cad18da19f4e523e28c02a04e
alexfrenkel92 <alexfrenkel92@gmail.com>
2020-02-24 11:58:27 +0100

    reddit frontent posting works

---
+++ week-09/5. nap - reddit be/reddit/public/index.js
@@ -0,0 +1,55 @@
+'use strict';
+
+// window.onload = () => {
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('GET','http://localhost:3000/posts')
+//     xhr.send();
+//     xhr.onload = (response) => {
+//         if (response.target.status == 200) {
+//             const mainContainer = document.querySelector('.mainContainer');
+//             let posts = JSON.parse(response.target.response);
+//             console.log(posts);
+            
+//             posts.forEach(post => {
+//                 let pTag = document.createElement('h2');
+//                 let urlTag = document.createElement('p')
+//                 pTag.innerText = post.title;
+//                 urlTag.innerText = post.URL;
+//                 mainContainer.appendChild(pTag);
+//                 mainContainer.appendChild(urlTag);
+//             })
+//         }
+//     }
+// }
+
+function getPost(method, url) {
+    return new Promise(() => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.send();
+        xhr.onload = (response) => {
+            if (response.target.status == 200) {
+                const mainContainer = document.querySelector('.mainContainer');
+                const posts = JSON.parse(response.target.response);
+                
+                posts.forEach(post => {
+                    const container = document.createElement('div');
+                        container.setAttribute('class', 'postContainer');
+                    const pTag = document.createElement('p');
+                        pTag.setAttribute('class', 'postTitle');
+                    const urlTag = document.createElement('p')
+                        urlTag.setAttribute('class', 'postUrl')
+                        
+                    pTag.innerText = `Title: ${post.title}`;
+                    urlTag.innerText = `url: ${post.URL}`;
+                    
+                    container.appendChild(pTag);
+                    container.appendChild(urlTag);
+                    mainContainer.appendChild(container);
+                })
+            }
+        }
+    })
+}
+
+getPost('GET', 'http://localhost:3000/posts');
