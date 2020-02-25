26862bf31fc778413873480452ae1002c19f893f
alexfrenkel92 <alexfrenkel92@gmail.com>
2020-02-25 15:19:36 +0100

    rdb, backend ready

--- week-10/Fox player/server.js
+++ week-10/Fox player/server.js
@@ -35,6 +35,7 @@ conn.connect((err) => {
     console.log('Database connected');
 });
 
+//TRACKS GTE POST DELETE
 app.get('/tracks', (req, res) => {
     conn.query('SELECT * FROM tracks;', (err, table) => {
         if (err) {
@@ -46,38 +47,119 @@ app.get('/tracks', (req, res) => {
     })
 })
 
-app.post('/post', (req, res) => {
+app.post('/post-track', (req, res) => {
     const videoId = req.body.videoId;
     fetchVideoInfo(`${videoId}`)
-    .then( (videoInfo) => {
-        console.log(videoInfo.title, videoInfo.owner, videoInfo.duration);
-        const id = 0;
-        const title = videoInfo.title;
-        const artist = videoInfo.owner;
-        const duration = videoInfo.duration;
-        const url = videoInfo.url;
-        conn.query(
-            `INSERT INTO tracks (title, artist, duration, url) VALUES (?, ?, ?, ?)`,
-            [title, artist, duration, url]);
-        (err) => {
-            if (err) {
-                res.status(500).send('Unknown table');
-                return null;
+        .then((videoInfo) => {
+            console.log(videoInfo.title, videoInfo.owner, videoInfo.duration);
+            const id = 0;
+            const title = videoInfo.title;
+            const artist = videoInfo.owner;
+            const duration = videoInfo.duration;
+            const url = videoInfo.url;
+            conn.query(
+                `INSERT INTO tracks (title, artist, duration, url) VALUES (?, ?, ?, ?)`,
+                [title, artist, duration, url]);
+            (err) => {
+                if (err) {
+                    res.status(500).send('Unknown table');
+                    return null;
+                }
             }
-        }
-        res.redirect('/tracks');
-    });
+            res.redirect('/tracks');
+        });
 })
 
-app.delete('/delete/:id', (req, res) => {
+app.delete('/delete-track/:id', (req, res) => {
     conn.query(
         `DELETE FROM tracks WHERE id = ${req.params.id}`,
         (err, result) => {
             if (err) {
-                res.status(500).send('Delete failed');
+                res.status(500).send('Cannot delete track');
                 return null;
             }
-            res.status(200).send(result);
+            res.status(200).redirect('/tracks');
+        }
+    )
+})
+
+//PLAYLISTS GET POST DELETE
+app.get('/playlists', (req, res) => {
+    conn.query(`SELECT * FROM playlists;`, (err, table) => {
+        if (err) {
+            console.log(`Cannot retrieve data: ${err.toStringify()}`);
+            res.sendStatus(500);
+            return null;
+        }
+        res.status(200).send(table);
+    })
+})
+
+app.post('/post-playlist', (req, res) => {
+    const playlistName = req.body.name;
+    conn.query(
+        `INSERT INTO playlists (name) VALUES (?)`,
+        [playlistName]);
+    (err) => {
+        if (err) {
+            res.status(500).send('Unkown table');
+            return null;
+        }
+    }
+    res.redirect('playlists');
+})
+
+app.delete('/delete-playlist/:id', (req, res) => {
+    conn.query(
+        `DELETE FROM playlists WHERE id = ${req.params.id}`,
+        (err, result) => {
+            if (err) {
+                res.status(500).send('Cannot delete playlist');
+                return null;
+            }
+            res.status(200).redirect('/playlists');
+        }
+    )
+})
+
+//GET, ADD, DELETE SONG to/from SPECIFIC PLAYLIST
+
+app.get('/playlists-tracks', (req, res) => {
+    conn.query(`SELECT * FROM playlists_tracks;`, (err, table) => {
+        if (err) {
+            console.log(`Cannot retrieve data: ${err.toString()}`);
+            res.sendStatus(500);
+            return null;
+        }
+        res.status(200).send(table);
+    })
+})
+
+app.post('/post-track-to-playlist', (req, res) => {
+    const trackid = req.body.trackid;
+    const playlistid = req.body.playlistid;
+    conn.query(
+        `INSERT INTO playlists_tracks (trackid, playlistid) VALUES (?, ?)`,
+        [trackid, playlistid]);
+        (err) => {
+            if (err) {
+                res.status(500).send('Unkown table');
+                return null;
+            }
+        }
+        res.status(200).redirect('/playlists-tracks');
+})
+
+app.delete('/delete-track-from-playlist/:trackid&&:playlistid', (req, res) => {
+    conn.query(
+        `DELETE FROM playlists_tracks WHERE
+        trackid = ${req.params.trackid} AND playlistid = ${req.params.playlistid};`,
+        (err, result) => {
+            if (err) {
+                res.status(500).send('Cannot delete track from playlist');
+                return null;
+            }
+            res.status(200).redirect('/playlists-tracks');
         }
     )
 })
